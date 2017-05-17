e978f3e712f8c1961d6eeff35e59ebd23b13c5dc
Natasha Seregina <natashaseregina@MacBook-Natasha.local>
2017-05-17 03:00:17 +0300

    prompt and alert

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,10 @@
+"use strict";
 $(document).ready(function() {
-  alert("Hi!");
+  var name = prompt("Как Вас зовут?", '');
+  if (name == null) {
+    alert("Жаль, что вы не захотели знакомиться!");
+  }
+  else {
+    alert("Привет, " + name + "!");
+  }
 });
